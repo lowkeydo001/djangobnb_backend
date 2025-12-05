51b6bf80c8452cf1bd8b0b8d5ba36f7a97fa41f0
Edsel <edsel.lorejo@msunaawan.edu.ph>
2025-12-05 20:58:39 +0800

    Video 5 - Detail Page for Properties - EDSEL LOREJO

--- property/models.py
+++ property/models.py
@@ -7,8 +7,11 @@ from django.db import models
 
 from useraccount.models import User
 
+
+
+
 class Property(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, serialize=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
     price_per_night = models.IntegerField()
@@ -18,9 +21,11 @@ class Property(models.Model):
     country = models.CharField(max_length=255)
     country_code = models.CharField(max_length=10)
     category = models.CharField(max_length=255)
+    # favorited = models.ManyToManyField(User, related_name='favorites', blank=True)
     image = models.ImageField(upload_to='uploads/properties')
     landlord = models.ForeignKey(User, related_name='properties', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
     def image_url(self):
         return f'{settings.WEBSITE_URL}{self.image.url}'
--- property/serializers.py
+++ property/serializers.py
@@ -4,7 +4,21 @@ from rest_framework import serializers
 from .models import Property
 
 
+from useraccount.serializers import UserDetailSerializer
+
+
+
+
+
+
 class PropertiesListSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
+
+    def get_image_url(self, obj):
+        return obj.image_url()
+
+
     class Meta:
         model = Property
         fields = (
@@ -13,3 +27,29 @@ class PropertiesListSerializer(serializers.ModelSerializer):
             'price_per_night',
             'image_url',
         )
+
+
+class PropertiesDetailSerializer(serializers.ModelSerializer):
+    landlord = UserDetailSerializer(read_only=True, many=False)
+    image_url = serializers.SerializerMethodField()
+
+
+    def get_image_url(self, obj):
+        return obj.image_url()
+
+
+    class Meta:
+        model = Property
+        fields = (
+            'id',
+            'title',
+            'description',
+            'price_per_night',
+            'image_url',
+            'bedrooms',
+            'bathrooms',
+            'guests',
+            'landlord'
+        )
+
+
