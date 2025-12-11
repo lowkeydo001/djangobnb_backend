4de22e8aaffb81df5fe39ac2c3514892a58ce308
Edsel <edsel.lorejo@msunaawan.edu.ph>
2025-12-11 08:05:44 +0800

    edsel lorejo

--- property/urls.py
+++ property/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 
 from . import api
 
-
-
-
 urlpatterns = [
     path('', api.properties_list, name='api_properties_list'),
     path('create/', api.create_property, name='api_create_property'),
