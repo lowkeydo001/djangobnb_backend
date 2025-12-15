05d21840ee6f4f807675221894509d8d56b32b19
Edsel <edsel.lorejo@msunaawan.edu.ph>
2025-12-15 19:34:51 +0800

    Video 7 -  Make Conversations Dynamic  -  Edsel Lorejo

--- djangobnb_backend/urls.py
+++ djangobnb_backend/urls.py
@@ -1,18 +1,14 @@
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static  
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Authentication routes
-    path('api/auth/', include('dj_rest_auth.urls')),
-    path('api/auth/', include('useraccount.urls')),
-    path('api/auth/register/', include('dj_rest_auth.registration.urls')),
-
-    # Your property routes
     path('api/properties/', include('property.urls')),
-]
+    path('api/auth/', include('useraccount.urls')),
+    path('api/chat/', include('chat.urls')),
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
